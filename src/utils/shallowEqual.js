b80ca17d9e7455d1c351cd023d94737b2d2c3f0e
杨魁 <c6swift@gmail.com>
2018-01-25 13:36:38 +0800

    Prefer use `Object.is` in shadowEqual
    prefer use `object.is`, and in the `is` polyfill, just remove the branch—`y !== 0` is ok.

--- src/utils/shallowEqual.js
+++ src/utils/shallowEqual.js
@@ -1,12 +1,15 @@
 const hasOwn = Object.prototype.hasOwnProperty
 
-function is(x, y) {
-  if (x === y) {
-    return x !== 0 || y !== 0 || 1 / x === 1 / y
-  } else {
-    return x !== x && y !== y
-  }
-}
+const is =
+  typeof Object.is === 'function'
+    ? Object.is
+    : (x, y) => {
+        if (x === y) {
+          return x !== 0 || 1 / x === 1 / y;
+        } else {
+          return x !== x && y !== y;
+        }
+      };
 
 export default function shallowEqual(objA, objB) {
   if (is(objA, objB)) return true
